2adbe32f95556dcc406b6748f3f43323578f253a
Sravanthi Nandini <sravanthinandinig@gmail.com>
2026-06-30 00:45:17 +0530

    fix vite config plugin import name

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,3 +8,4 @@ export default defineConfig({
     extensions: ['.js', '.jsx', '.json']
   }
 })
+    
